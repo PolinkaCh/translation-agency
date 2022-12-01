a6a69755c2a68e2be266071d44db3344ab7a5d7f
Polina <PolyCher_10@mail.ru>
2022-12-01 11:44:00 +0500

    changes in filter

--- filmslist/orders/views.py
+++ filmslist/orders/views.py
@@ -15,15 +15,30 @@ def profile(request):
 def myorders(request):
     user = request.user
     translator = Translators.objects.get(user_name=user)
-    order = Order.objects.filter(translator=translator).order_by('receipt_date')
+    order = Order.objects.filter(translator=translator)
+    if order is not None:
+        order = order.order_by('receipt_date')
+        queryset = order
     sum = 0
     for el in order:
         sum += el.rate * el.duration
 
     months = order.values('deadline')
-    queryset = Order.objects.filter(deadline__in=request.GET.getlist('months'))
+    #queryset = Order.objects.filter(deadline__in=request.GET.getlist('months'))
 
-    return render(request, 'myorders.html', {'order': order, 'sum': sum, 'months': months, 'queryset': queryset, "user": user})
+    mo = list()
+    m = request.GET.getlist('month')
+    if len(m) != 0:
+        for mon in m:
+            mo.append(mon.split(' ')[0].replace('.', ''))
+
+        queryset = list()
+        for o in order:
+            if o.deadline.strftime("%b") in mo:
+                queryset.append(o)
+
+    return render(request, 'myorders.html', {'order': order, 'sum': sum, 'months': months,
+                                             'queryset': queryset, "user": user})
 
 def neworders(request):
     newOrder = Post.objects.order_by('date_posted')
