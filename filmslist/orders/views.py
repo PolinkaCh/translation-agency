18804285be4290279de9e153bf90d1cdd1ff0d64
Polina <PolyCher_10@mail.ru>
2022-12-04 12:52:24 +0500

    fix bags

--- filmslist/orders/views.py
+++ filmslist/orders/views.py
@@ -15,24 +15,27 @@ def myorders(request):       #рендер страницы со взятыми 
     if order is not None:
         order = order.order_by('receipt_date')      #вывод всех выполненных пользователем заказов
         queryset = order
-    sum = 0
-    for el in order:
-        sum += el.rate * el.duration
 
     months = order.values('deadline')            #фильтр по месяцам
     mo = list()
     m = request.GET.getlist('month')
+
     if len(m) != 0:
         for mon in m:
             mo.append(mon.split(' ')[0].replace('.', ''))
 
         queryset = list()
+
         for o in order:
             if o.deadline.strftime("%b") in mo:
                 queryset.append(o)
 
-    return render(request, 'myorders.html', {'order': order, 'sum': sum, 'months': months,
-                                             'queryset': queryset, "user": user})
+    sum = 0
+    for el in queryset:
+        sum += el.rate * el.duration
+
+    return render(request, 'myorders.html', {'order': order, 'months': months, "sum": sum,
+                                             'queryset': queryset,  "user": user})
 
 def neworders(request):            #рендер страницы с новыми заказами
     newOrder = Post.objects.order_by('date_posted')
--- filmslist/users/models.py
+++ filmslist/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='6d0707b4fecda5e1f930d4a445b1afa9.jpeg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
